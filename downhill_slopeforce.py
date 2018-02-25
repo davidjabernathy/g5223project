c3fbeb473cbc62045964d59223867551463e344d
duncan1147 <33845914+duncan1147@users.noreply.github.com>
2018-02-25 17:38:10 -0500

    Update downhill_slopeforce.py

--- downhill_slopeforce.py
+++ downhill_slopeforce.py
@@ -4,6 +4,16 @@ import math
   // SKATE ACCELERATION/VELOCITY //
  //     GOING DOWNHILL CODE     //
 /////////////////////////////////
+
+The function will return 3 different variables, the skaters force going
+downhill, the downward angle of the hill, and the skater velocity.
+This is for visual outputs incase we want to show the angle of the hill
+as well as the speed the skater will go.
+
+
+NOTE: slope_force and skater_velocity are both showing the same thing
+just in different ways. Recommend the skater_velocity as it is in speed
+and not Newtons of force
 """
 
 # All variables we will need to have pull from the data
@@ -11,7 +21,7 @@ import math
 if needed, top of the hill elevation and botom of hill elevation code
 
 TopHill_elevation = 552 #feet
-bottomHill_elevation = 10
+bottomHill_elevation = 10 #feet
 HillHeight = TopHill_elevation - bottomHill_elevaion
 
 """
@@ -35,8 +45,8 @@ def skate_downhill_accel(street_length, skateweight, elevation, gravity):
 
     skate_velocity = street_length / skateTime
     
-    #secondary method that brings in Friction
     airfrict = .02
+    #Newtons of force
     slope_force = skateweight * hillangle * (9.81 - airfrict)
     
     return(slope_force, hillangle, skate_velocity)
